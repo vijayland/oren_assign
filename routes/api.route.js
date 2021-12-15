724e31a1330d0793f38cc16ce19331b55ca5706f
Vijay Ethuraj <vijayethuraj@gmail.com>
2021-12-15 14:48:45 +0530

    Added user login authentication

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 var bcryptjs = require('bcryptjs');
 const { PrismaClient } = require('@prisma/client')
+const jwt = require('jsonwebtoken');
 
 const prisma = new PrismaClient();
 
@@ -53,8 +54,60 @@ router.post('/user/register', async (req, res, next) => {
   }
 });
 
-router.get('/user/login', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+router.post('/user/login', async (req, res, next) => {
+  try {
+    let { email, password } = req.body;
+
+    const isValidUser = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    })
+
+    if (!isValidUser) {
+      return res.status(401).json({
+        msg: "Invalid Email...",
+        status: 404,
+      })
+    }
+
+    let isValidPassword = await bcryptjs.compare(password, isValidUser.password);
+
+    if (!isValidUser) {
+      res.send({
+        msg: "password don't match...",
+        'status_code': 404,
+      });
+    }
+
+    if (isValidPassword) {
+      let payload = {
+        user: {
+          id: isValidUser.id,
+          email: email
+        }
+      }
+
+      jwt.sign(
+        payload,
+        process.env.JWTSECRET,
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            status: 200,
+            tokenId: token,
+            user: {
+              email: isValidUser.email,
+              name: isValidUser.name,
+              address: isValidUser.address
+            }
+          })
+        })
+    }
+
+  } catch (err) {
+
+  }
 });
 
 router.get('/menu', async (req, res, next) => {
